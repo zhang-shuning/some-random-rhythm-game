bf73c78fd7df46ef4e1d9b40d09cb6547a844c13
zhang-shuning <ningnongidk@gmail.com>
2026-09-16 14:22:12 -0700

    White rectangle to test

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pygame
+import modules.classes
+import modules.scripts
 
 FPS_CAP = 60
+HORIZONTAL_SIZE = 800
+VERTICAL_SIZE = 600
 
 #Pygame variable initialization
 running:bool = True
+screen = pygame.display.set_mode((HORIZONTAL_SIZE, VERTICAL_SIZE))
 clock:pygame.time.Clock = pygame.time.Clock()
+rects_to_draw:list[pygame.Rect] = [
+    pygame.Rect(100,100,100,100)
+]
 
 while running:
     clock.tick(FPS_CAP)
+    for i in rects_to_draw:
+        pygame.draw.rect(screen, (255,255,255), i)
+    pygame.display.flip()
