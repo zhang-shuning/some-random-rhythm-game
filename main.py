03be62de5d7b7bae2c3734a2838e05e578948bc0
zhang-shuning <ningnongidk@gmail.com>
2026-09-17 23:30:01 -0700

    adds escape for quit

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
     #Get delta time events
     for i in delta_time_list:
         if i.is_true:
